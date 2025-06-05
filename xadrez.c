a87bdfd1da4fe5d9eaae0a4ed38d0e719279d2c6
EdsonJota <edsonfortes26@hotmail.com>
2025-06-05 13:05:54 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -29,5 +29,23 @@ int main() {
         r++;
     }while(r<8); // acho que é isso
 
+    // agora o cavalo
+    // ele anda tipo em L
+
+    printf("\nMovimento do Cavalo:\n");
+
+    int i, j;
+    for(i = 0; i < 1; i++) // só faz uma vez, pq é um L
+    {
+        j = 0;
+        while(j < 2) // duas pra baixo
+        {
+            printf("Baixo\n");
+            j++;
+        }
+
+        printf("Esquerda\n"); // uma pra esquerda
+    }
+
     return 0;
 }
